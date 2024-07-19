05abffbeb3fa753e336e6d8bbefc4978dd010a1f
jacinth91 <joy237@gmail.com>
2024-07-19 11:44:12 +0400

    final push

--- app/pages/pageDetails.tsx
+++ app/pages/pageDetails.tsx
@@ -9,11 +9,23 @@ export default async function PageDetails() {
     const {id} = router.query;
 
     const specificResponse = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
-    const postDetails = await specificResponse.json()
+    const postDetails :any[] = await specificResponse.json()
     return (
         <Container maxWidth="sm">
-         <div>
-            
+         <div className="w-6/12">
+          <p></p>  
+
+          <ul>
+            {postDetails.map((post)=>(
+              <li>
+                
+                <p>{post.email}</p>
+                <p>{post.userId}</p>
+                <p>{post.name}</p>
+                </li>
+              
+            ))}
+          </ul>
          </div>
         </Container>
       );
